37a4d74b10cf42a0d38a1ac5dd5ef08466920bd4
Andrew Cove <andrew.cove@gmail.com>
2010-12-29 23:36:54 -0800

    A whole bunch more checks.  Need to happen on updates.  Need to precompute and cache multi-selectors.

--- eternal.js
+++ eternal.js
@@ -3,16 +3,46 @@ var ids = [
 	   ];
 
 $(ids).each(function() {
-	var id = this;
-	//remove from stream
-	var items = jQuery(".uiStreamStory[data-ft]");
-	items.each(function() {
-		var data = jQuery.parseJSON(jQuery(this).attr("data-ft"));
-		if (data != null && data.actrs == id)
-		    jQuery(this).hide()
-	    });
-	//remove from thumbnail updates
-	//remove from profile updates
-	//remove from chatlist
-	//remove from frequent chat list
+    //TODO: don't have to loop over ids.
+    //can use multiple selectors (separated by ',')
+
+    var id = this;
+    //remove from stream
+    var streamItems = jQuery(".uiStreamStory[data-ft]");
+    streamItems.each(function() {
+	var data = jQuery.parseJSON(jQuery(this).attr("data-ft"));
+	if (data != null && data.actrs == id)
+	    jQuery(this).hide();
+    });
+
+    //remove from profile photo updates
+    var ppSelector = '#pagelet_friends_recentlyupdated .phs:first img[src*=_'+id+'_]';
+    debugger
+    var ppItems = jQuery(ppSelector);
+    ppItems.each(function(){
+	jQuery(this).parent().parent().parent().parent().parent().hide();
+    });
+
+    //remove from profile updates
+    var puItems = jQuery('ul.uiList.friendsDashboard img[src*=_'+id+'_]');
+    puItems.each(function(){
+	jQuery(this).parent().parent().parent().hide();
+    });
+
+    //remove from profile friends list
+    var pfItems = jQuery('ul.profile-friends img[src*=_'+id+'_]');
+    pfItems.each(function(){
+	jQuery(this).parent().parent().parent().hide();
+    });
+    //remove from chatlist
+    var chatItems = jQuery("a[id*=buddy_list_item_"+id+"]");
+    chatItems.each(function(){
+	debugger
+	jQuery(this).hide();
+    });
+    //remove from frequent chat list
+    var fcSelector = '#chatFriendsOnline img[src*=_'+id+'_]';
+    var fcItems = jQuery(fcSelector);
+    fcItems.each(function(){
+	jQuery(this).parent().parent().hide();
     });
