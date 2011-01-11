819ed4e829bef2b889257e37fac807ba37dee85e
Andrew Cove <andrew.cove@gmail.com>
2011-01-11 00:20:18 -0800

    shared function for adding elements to blocked friend list.  sets class on li.

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 var backgroundPage;
 
+function createListItem(v)
+{
+    var li = document.createElement("li");
+    li.innerHTML = v;
+    jQuery(li).attr("class", "blocked_friend");
+    return li;
+}
+
 function buildList(hiddenFriends)
 {
     jQuery("#blocked_friends").append(hiddenFriends.map(function(v){
-        var li = document.createElement("li");
-        li.innerHTML = v;
-	return li;
+        return createListItem(v);
     }));
 }
 
@@ -13,14 +19,12 @@ jQuery(document).ready(function(){
     backgroundPage = chrome.extension.getBackgroundPage();
     buildList(backgroundPage.getHiddenFriends());
     var form = jQuery("#hide_friend_form");
+    //var img = document.create("img");
+    //jQuery(img).src = "hide_button.png";
     form.submit(function(event){
 	event.preventDefault();
 	var val = jQuery("#id").val();
 	backgroundPage.addHiddenFriend(val);
-	var li = document.createElement("li");
-        li.innerHTML = val;
-        jQuery("#blocked_friends").append(li);	
-	/*
-	*/
+        jQuery("#blocked_friends").append(createListItem(val));
     });
 });
